d1eee6c896ea6edac4e8ae2ccb205eae443de929
TheTemportalist <the.country.gamer@gmail.com>
2015-02-08 14:50:59 -0500

    Working with scala issues

--- src/main/scala/test/EntityTestS.scala
+++ src/main/scala/test/EntityTestS.scala
@@ -1,23 +1,21 @@
 package test
 
-import net.minecraft.command.CommandResultStats.Type
+import net.minecraft.command.ICommandSender
+import net.minecraft.entity.Entity
 import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.world.World
-import net.minecraft.entity.Entity
 
 /**
  *
  *
  * @author TheTemportalist 2/8/15
  */
-class EntityTestS(world: World) extends Entity(world) {
-
-	override def setCommandStat(`type`: Type, amount: Int): Unit = {}
+class EntityTestS(world: World) extends Entity(world) with ICommandSender {
 
 	override def entityInit(): Unit = {}
 
 	override def writeEntityToNBT(tagCompound: NBTTagCompound): Unit = {}
 
-	override def readEntityFromNBT(tagCompund: NBTTagCompound): Unit = {}
+	override def readEntityFromNBT(tagCompound: NBTTagCompound): Unit = {}
 
 }
